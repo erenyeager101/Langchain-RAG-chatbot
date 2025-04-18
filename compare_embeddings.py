d5558ed47426855dec75d2b87c011ec62cccd22d
Kunal Sonne <153419118+erenyeager101@users.noreply.github.com>
2025-04-18 12:18:25 +0530

    Update compare_embeddings.py

--- compare_embeddings.py
+++ compare_embeddings.py
@@ -4,11 +4,7 @@ from dotenv import load_dotenv
 import openai
 import os
 
-# Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
-#---- Set OpenAI API key 
-# Change environment variable name from "OPENAI_API_KEY" to the name given in 
-# your .env file.
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 def main():
